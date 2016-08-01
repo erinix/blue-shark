96c148bee7dec10ec3e2c2e79ba20bab9613f80b
Thibault Zanini <thibaultzanini@gmail.com>
2016-08-01 15:49:30 -0700

    Refactor checkbox component. - Reduce the number of bindings. (3 less) - Improve memory consumption and performance. - Improve loading time.

--- ui/checkbox.reel/checkbox.js
+++ ui/checkbox.reel/checkbox.js
@@ -1,43 +1,10 @@
 /**
  * @module ui/checkbox.reel
  */
-var Component = require("montage/ui/component").Component,
-    Uuid = require("montage/core/uuid");
+var MontageCheckBox = require("montage/ui/checkbox.reel").Checkbox;
 
 /**
  * @class Checkbox
  * @extends Component
  */
-exports.Checkbox = Component.specialize({
-    enabled: {
-        value: true
-    },
-
-
-    _checked: {
-        value: false
-    },
-
-    checked: {
-        set: function (checked) {
-            this._checked = !!checked;
-        },
-        get: function () {
-            return this._checked;
-        }
-    },
-
-    _uuid: {
-        value: null
-    },
-
-    enterDocument: {
-        value: function (isFirstTime) {
-            if (isFirstTime) {
-                this._uuid = Uuid.generate();
-                this.checkboxComponent.element.setAttribute("id", this._uuid);
-                this.labelComponent.element.setAttribute("for", this._uuid);
-            }
-        }
-    }
-});
+exports.Checkbox = MontageCheckBox.specialize();
